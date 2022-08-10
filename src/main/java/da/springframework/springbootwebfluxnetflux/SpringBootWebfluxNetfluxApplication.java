21cfe10bf7ba3d028e1d01b566e465e7586bb631
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-08-10 09:43:46 -0500

    19. Initial Commit.

---
+++ src/main/java/da/springframework/springbootwebfluxnetflux/SpringBootWebfluxNetfluxApplication.java
@@ -0,0 +1,13 @@
+package da.springframework.springbootwebfluxnetflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebfluxNetfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebfluxNetfluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/da/springframework/springbootwebfluxnetflux/SpringBootWebfluxNetfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package da.springframework.springbootwebfluxnetflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxNetfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
